cae26179a9fa363137a8dacd72d9c07ef903d49b
stepanhdjrZ <stepan.zhdanov.12@bk.ru>
2026-05-09 17:10:11 +0500

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,9 @@
+﻿import PackageDescription
+import AppleProductTypes
+
+let package = Package(
+    name: "MeshChat",
+    platforms: [.iOS("16.0")],
+    products: [.iOSApplication(name: "MeshChat", targets: ["AppModule"], displayVersion: "1.0", bundleVersion: "1", appIcon: .placeholder(icon: .chat), supportedDeviceFamilies: [.iphone], supportedInterfaceOrientations: [.portrait], capabilities: [.bluetoothAlways(purposeString: "Связь без интернета"), .localNetwork(purposeString: "Связь в Wi-Fi")])],
+    targets: [.executableTarget(name: "AppModule", path: "Sources")]
+)
