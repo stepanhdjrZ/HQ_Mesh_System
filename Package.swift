07d5a0a4e24249c32261dc8aec4efc4ca44ca331
stepanhdjrZ <stepan.zhdanov.12@bk.ru>
2026-05-13 22:13:22 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "HQGlobalMesh",
-    platforms: [
-        .iOS(.v15) // Поддержка твоего iPhone 8 и выше
-    ],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "HQGlobalMesh", targets: ["HQGlobalMesh"])
     ],
     dependencies: [
-        // Тот самый Firebase, который валит билды
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToDateWithMiniVersion(from: "10.0.0"))
     ],
     targets: [
@@ -21,7 +18,8 @@ let package = Package(
                 .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
             ],
-            path: "." // Гитхаб будет искать код во всех папках
+            path: ".",
+            exclude: ["Package.swift", "GoogleService-Info.plist"] // Исключаем не-код файлы
         )
     ]
 )
